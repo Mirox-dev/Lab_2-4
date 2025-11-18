a0032761a018a5625b2eb786916d72feff16e5ee
Mirox <egevgolub@edu.hse.ru>
2025-11-18 21:34:08 +0300

    Lab 2_3 done with checks

--- Lab2_3.cpp
+++ Lab2_3.cpp
@@ -5,37 +5,48 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <limits>
 
 using namespace std;
 
-void inputData(float* const p, int n) {
-    cout << "Введите значения: ";
-    string s;
-    cin >> s;
-    istringstream ss(s);
-    for (int i = 0; i < n; i++) {
-        if (!(ss >> *(p + i))) {
-            ss >> *(p + i);
-            if (-50 <= *(p + i) <= 50) {
-                cout << "Неверный ввод";
-                exit(0);
+void inputData(float* const p, int n){
+    while (true) {
+        cout << "Enter values:  ";
+        string s;
+        getline(cin, s);
+        istringstream ss(s);  // Важно
+        bool exit = true;
+        for (int i = 0; i < n; i++) {
+            if (!(ss >> *(p + i))) {
+                cout << "Wrong input, try again" << endl;
+                exit = false;
+                break;
+            }
+            if (*(p + i) < -50 or *(p + i) > 50) {
+                cout << "Wrong input, try again" <<endl;
+                exit = false;
+                break;
             }
         }
-        else {
-            exit(0);
+        float one_more;
+        if (exit && ss >> one_more) {
+            cout << "Wrong input, try again" << endl;
+            exit = false;
         }
+        if (exit) break;
     }
 }
+
 float average(const float* p, int n) {
     float avg = 0.0;
     for (int i = 0; i < n; i++) {
         avg += *(p + i);
     }
-    return (avg / static_cast<float>(n));
+    return (avg / static_cast<float>(n));    // Важно
 }
 
 float minValue(const float* p, int n) {
-    float min = *(p + 0);
+    float min = *p;
     for (int i = 0; i < n; i++) {
         if (min > *(p + i)) {
             min = *(p + i);
@@ -45,7 +56,7 @@ float minValue(const float* p, int n) {
 }
 
 float maxValue(const float* p, int n) {
-    float max = *(p + 0);
+    float max = *p;
     for (int i = 0; i < n; i++) {
         if (max < *(p + i)) {
             max = *(p + i);
@@ -54,8 +65,9 @@ float maxValue(const float* p, int n) {
     return max;
 }
 
-float* filterAboveAverage(float* const p, int n, int& newCount) {
+float* filterAboveAverage(const float* p, int n, int& newCount) {
     float avg = average(p, n);
+    newCount = 0;
     for (int i = 0; i < n; i++) {
         if (*(p + i) > avg) {
             newCount++;
@@ -74,9 +86,10 @@ float* filterAboveAverage(float* const p, int n, int& newCount) {
 
 int main() {
     int n;
-    cout << "=== Анализ сенсора ===" << endl;
-    cout << "Введите количество измерений: ";
+    cout << "=== Sensor analysis ===" << endl;
+    cout << "Enter the number of measurements: ";
     cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     auto* data = new float[n];
     inputData(data, n);
     float avg = average(data, n);
@@ -84,9 +97,19 @@ int main() {
     float max = maxValue(data, n);
     int newCount;
     float* aboveAvg = filterAboveAverage(data, n, newCount);
-    for (int i = 0; i < n; i++) {
-        cout << *(data + i);
+    cout << "Average value is: " << avg << endl;
+    cout << "Minimum value is: " << min << endl;
+    cout << "Maximum value is: " << max << endl;
+    cout << "Above average values are: ";
+    if (newCount == 0) {
+        cout << "No measurements found" << endl;
     }
+    else {
+        for (int i = 0; i < newCount; i++) {
+            cout << *(aboveAvg + i) << " ";
+        }
+    }
+    cout << endl;
     delete[] data;
     delete[] aboveAvg;
     return 0;
